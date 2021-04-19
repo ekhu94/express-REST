491ba69f3c9447a8c6b2dd529a22142285b34eda
Erik Huang <ekhu94@gmail.com>
2021-04-19 15:15:43 -0700

    parses req.body into key-value pairs on post request

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
@@ -13,4 +14,17 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
+});
+
+app.get("/tacos/new", (req, res) => {
+  res.render("tacoForm", { title: "Taco Form" });
+});
+
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+  const { meat, qty } = req.body;
+  res.send(`Order received! Here's your ${qty} ${meat} tacos.`);
 });
