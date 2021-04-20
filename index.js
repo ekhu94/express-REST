c2eb4195db68212323b2b641571bbefc2339c14b
Erik Huang <ekhu94@gmail.com>
2021-04-19 18:30:58 -0700

    successfully serves show page on specific id

--- index.js
+++ index.js
@@ -18,18 +18,22 @@ app.get("/", (req, res) => {
 
 const comments = [
   {
+    id: 1,
     username: "K-LAWN",
     content: "hmm.....that's weird....",
   },
   {
+    id: 2,
     username: "JasperDav",
     content: "I'm pumped up y'all!",
   },
   {
+    id: 3,
     username: "radiantcheese",
     content: "I wouldn't change anything about me...because I'm perfect.",
   },
   {
+    id: 4,
     username: "DDR_BOI",
     content: "do you guys wanna see my DDR shoes?",
   },
@@ -39,13 +43,22 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { comments, title: "Comments" });
 });
 
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === parseInt(id));
+  if (comment) {
+    res.render("comments/show", { title: comment.id, comment });
+  }
+});
+
 app.get("/comments/new", (req, res) => {
   res.render("comments/new", { title: "New Comment" });
 });
 
 app.post("/comments", (req, res) => {
+  const nextId = comments.length;
   const { username, content } = req.body;
-  comments.push({ username, content });
+  comments.push({ id: nextId, username, content });
   res.redirect("/comments");
 });
 
